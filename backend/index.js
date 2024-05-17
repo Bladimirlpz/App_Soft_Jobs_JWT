c3a3aff742958d28d45e014133145aeadaa09027
Bladimirlpz <bladimir.lopez.p@gmail.com>
2024-05-16 23:04:36 -0400

    Creacion de ruta get para obtener datos del usuario validando por el token

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
+require('dotenv').config({ path: './.env' })
 const { verificarUsuario, registrarUsuario, obtenerUsuario } = require('./consultas')
+// Middleware para valirdar existencia de token
+const { authMiddleware } = require('./middlewares/auth.middleware')
+// Middleware para generar reportes a las rutas y consultas
+const { generarReportes } = require('./middlewares/generarReportes')
 
 const app = express()
 app.use(cors())
@@ -10,34 +15,46 @@ app.use(express.json())
 app.listen(3000, console.log('Servidor OK'))
 
 // Get para traer informacion del usuario
-app.get('/usuarios', async (req, res) => {
+app.get('/usuarios', authMiddleware, generarReportes, async (req, res) => {
   try {
-    const usuario = await obtenerUsuario()
-    res.json(usuario)
+    const authorization = req.headers.authorization.split(' ')
+    const token = authorization[1]
+    const { email } = jwt.verify(token, process.env.JWT_SECRET)
+    const respuesta = await obtenerUsuario(email)
+    res.status(201).json([{
+      id: respuesta[0].id,
+      email: respuesta[0].email,
+      rol: respuesta[0].rol,
+      lenguage: respuesta[0].lenguage,
+      message: 'Bienvenido'
+    }])
   } catch (error) {
-    res.status(204).send('Datos no encontrados')
+    res.status(500).send({ message: 'Datos no encontrados' })
   }
 })
 
 // Post para login usuarios
-app.post('/login', async (req, res) => {
+app.post('/login', generarReportes, async (req, res) => {
   try {
     const { email, password } = req.body
-    await verificarUsuario(email, password)
-    const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '10h' })
-    res.send(token)
+    const token = await verificarUsuario(email, password)
+    res.status(200).json({ message: 'Token', token })
   } catch (error) {
-    res.status(500).send('Usuario o contraseña incorrecta')
+    res.status(401).json({ message: 'Usuario no encontrado' })
   }
 })
 
 // Post registrar usuarios
-app.post('/usuarios', async (req, res) => {
+app.post('/usuarios', generarReportes, async (req, res) => {
   try {
     const usuario = req.body
-    await registrarUsuario(usuario)
-    res.send('Usuario creado con exito')
+    const respuesta = await registrarUsuario(usuario)
+    res.status(201).json({
+      id: respuesta[0].id,
+      email: respuesta[0].email,
+      message: 'Agregado con exito'
+    })
   } catch (error) {
-    res.status(500).send('Error al registrar')
+    res.status(500).json({ message: 'Internal error server' })
   }
 })
